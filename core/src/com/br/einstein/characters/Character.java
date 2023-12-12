7f44ed7b141c151fb300f61d32abc474b9182a79
RodSoares1506 <rodsoaresnasc@hotmail.com>
2023-12-12 14:19:22 -0300

    ## Punch and Kick sounds added

--- core/src/com/br/einstein/characters/Character.java
+++ core/src/com/br/einstein/characters/Character.java
@@ -2,6 +2,7 @@ package com.br.einstein.characters;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -46,6 +47,8 @@ public class Character {
     private TextureRegion jumping;
     public Rectangle damageHitBox = new Rectangle();
     public Rectangle movementHitBox = new Rectangle();
+    Sound Punch = Gdx.audio.newSound(Gdx.files.internal("assets/Sounds/Punch.mp3"));
+    Sound Kick = Gdx.audio.newSound(Gdx.files.internal("assets/Sounds/Kick.mp3"));
 
     public Character(float x, float y, int left, int right, int space, int punch, int kick, int characterId) {
         this.x = x;
@@ -57,7 +60,7 @@ public class Character {
         this.punch = punch;
         this.kick = kick;
         this.characterId = characterId;
-        velocity = new Vector2(0, -1); // Define a velocidade inicial como -1 na direção Y (gravidade para baixo).
+        velocity = new Vector2(0, -1); // Define a velocidade inicial como -1 na direção Y (gravidade para baixo)
 
         if (characterId == 1) {
             punchImage = new Texture("assets/IracemaSprites/Iracema_soco_animation_D.png");
@@ -277,12 +280,14 @@ public class Character {
         if (isPunching) {
             if (stateTime >= punchAnimation.getAnimationDuration()) {
                 isPunching = false;
+                Punch.play(1.0f);
             } else {
                 return currentPunchFrame;
             }
         } else if (isKicking) {
             if (stateTime >= kickAnimation.getAnimationDuration()) {
                 isKicking = false;
+                Kick.play(1.0f);
             } else {
                 return currentKickFrame;
             }
